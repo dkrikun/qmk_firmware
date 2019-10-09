0d4153f1d1f5de03f5710b5ced8a8852e1e9fed8
Daniel Krikun <krikun.daniel@gmail.com>
2019-10-09 22:35:07 +0300

    Update dkrikun0 kbd75 keymap

--- keyboards/kbdfans/kbd75/keymaps/dkrikun0/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/dkrikun0/keymap.c
@@ -1,9 +1,27 @@
 #include QMK_KEYBOARD_H
+
 #define _BL 0
 #define _FL 1
+#define _ML 2
 
 #define XX_LSFT OSM(MOD_LSFT)
 #define XX_RSFT OSM(MOD_RSFT)
+
+// tap dance
+enum {
+  TD_LSFT_CAPS = 0,
+  TD_RSFT_CAPS = 1,
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+  [TD_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS),
+};
+
+#define TD_LSFT TD(TD_LSFT_CAPS)
+#define TD_RSFT TD(TD_LSFT_CAPS)
+
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -12,17 +30,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_DEL,   KC_BSPC,  KC_HOME,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
     MO(_FL),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN,
-    XX_LSFT,  _______,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
-    KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,   KC_MENU,  KC_BSPC,                      KC_RALT,  MO(_FL),  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    TD_LSFT,  _______,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  TD_RSFT,            KC_UP,    KC_END,
+    KC_LCTL,  KC_LGUI,  KC_LALT,                      KC_SPC,   KC_MENU,  KC_BSPC,                      KC_RALT,  TT(_ML),  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [_FL] = LAYOUT(
-    _______,  KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,   KC_F22,   KC_F23,   KC_F24,   KC_PAUS,  KC_SLCK,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_BRID,  KC_BRIU,  _______,  _______,  KC_MUTE,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_VOLU,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  KC_VOLD,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_BTN1,            KC_MS_U,  KC_BTN2,
-    RESET,    _______,  _______,                      _______,  RGB_TOG,  _______,                      _______,  _______,  _______,  KC_MS_L,  KC_MS_D,  KC_MS_R
+    _______,  KC_F13,   KC_F14,   KC_F15,   KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,   KC_F22,   KC_F23,   KC_F24,   KC_PAUS,  KC_SLCK,  KC_MUTE,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_VOLU,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_VOLD,
+    _______,  _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,                      _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,
+    RESET,    _______,  _______,                      KC_ENT,   RGB_TOG,  _______,                      _______,  _______,  _______,  _______,  _______,  _______
+  ),
+
+  [_ML] = LAYOUT(
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MS_WH_LEFT,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            KC_MS_WH_UP,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      KC_BTN3,  KC_MS_WH_DOWN,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_BTN2,            KC_MS_U,  KC_MS_WH_RIGHT,
+    _______,  _______,  _______,                      _______,  _______,  _______,                      _______,  _______,  KC_BTN1,  KC_MS_L,  KC_MS_D,  KC_MS_R
   ),
 };
 
@@ -30,19 +57,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // static, white light by default
 void keyboard_post_init_user(void) {
     rgblight_disable_noeeprom();
-    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+    rgblight_mode_noeeprom(1);
     rgblight_sethsv_noeeprom_white();
 }
 
-// set red light upon reset upcode
-bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-    switch(keycode) {
-        case RESET:
-            rgblight_enable_noeeprom();
-            rgblight_sethsv_noeeprom_red();
-            return true;
-
-        default:
-            return true;
-    }
+void shutdown_user(void) {
+    rgblight_enable_noeeprom();
+    rgblight_mode_noeeprom(1);
+    rgblight_sethsv_noeeprom_red();
 }
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case _ML:
+        rgblight_sethsv_noeeprom_teal();
+        break;
+    default:
+        rgblight_sethsv_noeeprom_white();;
+        break;
+    }
+  return state;
+}
+
